e7f6440d00be387851f79e8e3976b91768c50a37
Ezzat Mohamed <73354781+ezat141@users.noreply.github.com>
2024-04-21 04:24:31 +0200

    edit validateCategory

--- middleware/validationMiddleware.js
+++ middleware/validationMiddleware.js
@@ -20,7 +20,6 @@ exports.validateProduct = [
 exports.validateCategory = [
     body('category_name').trim().isLength({ min: 1 }).withMessage('Category name is required'),
     body('category_name_ar').trim().isLength({ min: 1 }).withMessage('Category name (Arabic) is required'),
-    body('image').trim().isLength({ min: 1 }).withMessage('Image is required'),
     (req, res, next) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
