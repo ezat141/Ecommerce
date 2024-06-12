7664dd55ac6a729c965cd4beecde42127d6919c7
Ezzat Mohamed <73354781+ezat141@users.noreply.github.com>
2024-06-13 00:24:18 +0300

    create  removeFromFavorites endpoint

--- controllers/favoriteController.js
+++ controllers/favoriteController.js
@@ -36,17 +36,17 @@ exports.addToFavorites = async (req, res) => {
 // Controller function to remove a product from favorites
 exports.removeFromFavorites = async (req, res) => {
     try {
-        const { favorite_usersid, favorite_productsid } = req.body;
+        const { favorite_id } = req.body;
 
         // Check if the favorite exists
-        const existingFavorite = await Favorite.findOne({ favorite_usersid, favorite_productsid });
+        const existingFavorite = await Favorite.findById(favorite_id);
 
         if (!existingFavorite) {
             return res.status(400).json({ success: false, message: 'Product not found in favorites' });
         }
 
         // Remove the favorite
-        await Favorite.deleteOne({ favorite_usersid, favorite_productsid });
+        await Favorite.deleteOne({ _id: favorite_id});
 
         // Update the favorite field in the Product model
         // await Product.updateOne({ _id: favorite_productsid }, { favorite: false });
@@ -58,80 +58,6 @@ exports.removeFromFavorites = async (req, res) => {
     }
 };
 
-// exports.getFavoritesWithDetails = async (req, res) => {
-//     try {
-//         const { favorite_usersid } = req.body;
-
-//         // Check if the user exists
-//         const userExists = await User.exists({ _id: favorite_usersid });
-//         if (!userExists) {
-//             return res.status(400).json({ success: false, message: 'User not found' });
-//         }
-
-//         // Aggregate to get favorites with product and user details
-//         const favoritesWithDetails = await Favorite.aggregate([
-//             {
-//                 $match: { favorite_usersid: favorite_usersid }
-//             },
-//             {
-//                 $lookup: {
-//                     from: 'products',
-//                     localField: 'favorite_productsid',
-//                     foreignField: '_id',
-//                     as: 'product'
-//                 }
-//             },
-//             {
-//                 $unwind: '$product'
-//             },
-//             {
-//                 $lookup: {
-//                     from: 'users',
-//                     localField: 'favorite_usersid',
-//                     foreignField: '_id',
-//                     as: 'user'
-//                 }
-//             },
-//             {
-//                 $unwind: '$user'
-//             },
-//             {
-//                 $project: {
-//                     _id: 1,
-//                     favorite_usersid: 1,
-//                     favorite_productsid: 1,
-//                     product: {
-//                         _id: '$product._id',
-//                         product_name: '$product.product_name',
-//                         product_name_ar: '$product.product_name_ar',
-//                         product_desc: '$product.product_desc',
-//                         product_desc_ar: '$product.product_desc_ar',
-//                         image: '$product.image',
-//                         product_count: '$product.product_count',
-//                         product_active: '$product.product_active',
-//                         product_price: '$product.product_price',
-//                         product_discount: '$product.product_discount',
-//                         product_date: '$product.product_date',
-//                         product_cat: '$product.product_cat'
-//                         // Add other product fields as needed
-//                     },
-//                     user: {
-//                         _id: '$user._id',
-//                         // Add other user fields as needed
-//                     }
-//                 }
-//             }
-//         ]);
-//         if (!favoritesWithDetails || favoritesWithDetails.length === 0) {
-//             return res.status(404).json({ success: false, message: 'No favorites found for the user' });
-//         }
-
-//         res.status(200).json({ status: httpStatusText.SUCCESS, data: favoritesWithDetails });
-//     } catch (error) {
-//         console.error('Error fetching favorites with details:', error);
-//         res.status(500).json({ success: false, message: 'Internal Server Error' });
-//     }
-// };
 exports.getFavoritesWithDetails = async (req, res) => {
     try {
         const { favorite_usersid } = req.body;
