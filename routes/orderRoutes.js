b64fcaaf7e0bae3a6608823649122c1b56436ade
Ezzat Mohamed <73354781+ezat141@users.noreply.github.com>
2024-06-20 05:54:15 +0300

    refresh

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const orderController = require('../controllers/orderController');
-const { authenticate } = require('../middleware/authenticate');
-
 // Checkout
 router.post('/checkout', orderController.checkout);
 
