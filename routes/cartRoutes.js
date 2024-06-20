4f3a08852b6501c10b1bf8e61606d352fe7a8c8e
Ezzat Mohamed <73354781+ezat141@users.noreply.github.com>
2024-06-20 06:04:08 +0300

    refresh

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -1,16 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const cartController = require("../controllers/cartController");
-const { authMiddleware } = require("../middleware/authMiddleware");
 
 // Route to get the cart for a user
 router.post("/getCart", cartController.getCart);
 
 // Route to add a product to the cart
 router.post("/addToCart", cartController.addToCart);
-
-// Route to update the quantity of a product in the cart
-router.post("/updateCartItem", cartController.updateCartItem);
 
 // Route to remove a product from the cart
 router.post("/removeCartItem", cartController.removeCartItem);
