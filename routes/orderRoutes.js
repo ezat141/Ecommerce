540d986ab9d7c6803f28dd080bf24963e198abbd
Ezzat Mohamed <73354781+ezat141@users.noreply.github.com>
2024-06-24 03:07:33 +0300

    refresh viewOrders

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,7 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const orderController = require('../controllers/orderController');
+const orderController = require("../controllers/orderController");
 // Checkout
-router.post('/checkout', orderController.checkout);
+router.post("/checkout", orderController.checkout);
+
+router.post("/getOrders", orderController.viewOrders);
 
 module.exports = router;
