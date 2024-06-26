2e96f8fab03f9aea85e1887bffb34ad599ae9409
Ezzat Mohamed <73354781+ezat141@users.noreply.github.com>
2024-06-26 03:46:01 +0300

    viewOrders edited

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -64,14 +64,36 @@ exports.checkout = async (req, res) => {
 };
 
 exports.viewOrders = async (req, res) => {
-    const {orders_usersid}  = req.body;
+    const { orders_usersid } = req.body;
     try {
-        const orders = await Order.find({orders_usersid});
-        res.status(200).json({ status: httpStatusText.SUCCESS, data: orders });
-    } catch (error) {
-        res.status(500).json({ status: httpStatusText.FAIL, message: error.message });
-    }
+        const orders = await Order.find({ orders_usersid }).populate('orders_addressid');
 
+        const formattedOrders = orders.map(order => ({
+            _id: order._id,
+            orders_id: order.orders_id,
+            orders_usersid: order.orders_usersid,
+            orders_addressid: order.orders_addressid._id,
+            orders_type: order.orders_type,
+            orders_pricedelivery: order.orders_pricedelivery,
+            orders_price: order.orders_price,
+            orders_totalprice: order.orders_totalprice,
+            orders_couponid: order.orders_couponid,
+            orders_paymeentmethod: order.orders_paymeentmethod,
+            orders_status: order.orders_status,
+            orders_datetime: order.orders_datetime,
+            address_id: order.orders_addressid._id,
+            address_usersid: order.orders_addressid.address_usersid,
+            address_name: order.orders_addressid.address_name,
+            address_city: order.orders_addressid.address_city,
+            address_street: order.orders_addressid.address_street,
+            address_lat: order.orders_addressid.address_lat,
+            address_long: order.orders_addressid.address_long,
+        }));
+
+        res.status(200).json({ status: 'success', data: formattedOrders });
+    } catch (error) {
+        res.status(500).json({ status: 'fail', message: error.message });
+    }
 };
 
 exports.ordersDetailsView = async (req, res) => {
