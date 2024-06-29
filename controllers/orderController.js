c86e75a51357a3ae9cc5ce1eb152de07c0ae80e7
Ezzat Mohamed <73354781+ezat141@users.noreply.github.com>
2024-06-29 16:48:03 +0300

    refresh deleteOrders and archiveOrders

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -162,7 +162,7 @@ exports.deleteOrders = async (req, res) => {
             return res.status(400).json({ status: httpStatusText.FAIL, message: 'Cannot delete order with status other than 0' });
         }
 
-        await order.remove();
+        await Order.deleteOne({orders_id: orders_id});
 
         res.status(200).json({ status: httpStatusText.SUCCESS, message: 'Order deleted successfully' });
     } catch (error) {
@@ -174,7 +174,7 @@ exports.archiveOrders = async (req, res) => {
     const { orders_usersid } = req.body;
 
     try {
-        const orders = await Order.find({ orders_usersid, orders_status: 4 });
+        const orders = await Order.find({ orders_usersid, orders_status: 4 }).populate('orders_addressid');
 
         if (orders.length === 0) {
             return res.status(404).json({ status: httpStatusText.FAIL, message: 'No orders to archive' });
@@ -187,7 +187,30 @@ exports.archiveOrders = async (req, res) => {
         //     await order.save();
         // }
 
-        res.status(200).json({ status: httpStatusText.SUCCESS, message: 'Orders archived successfully' });
+        // Prepare the response data
+        const responseData = orders.map(order => ({
+            _id: order._id,
+            orders_id: order.orders_id,
+            orders_usersid: order.orders_usersid,
+            orders_addressid: order.orders_addressid._id,
+            orders_type: order.orders_type,
+            orders_pricedelivery: order.orders_pricedelivery,
+            orders_price: order.orders_price,
+            orders_totalprice: order.orders_totalprice,
+            orders_couponid: order.orders_couponid,
+            orders_paymeentmethod: order.orders_paymeentmethod,
+            orders_status: order.orders_status,
+            orders_datetime: order.orders_datetime,
+            address_id: order.orders_addressid._id,
+            address_usersid: order.orders_addressid.address_usersid,
+            address_name: order.orders_addressid.address_name,
+            address_city: order.orders_addressid.address_city,
+            address_street: order.orders_addressid.address_street,
+            address_lat: order.orders_addressid.address_lat,
+            address_long: order.orders_addressid.address_long
+        }));
+
+        res.status(200).json({ status: httpStatusText.SUCCESS, data: responseData });
     } catch (error) {
         res.status(500).json({ status: httpStatusText.FAIL, message: error.message });
     }
