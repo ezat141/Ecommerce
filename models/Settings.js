c0c817638febfa70912aca2beb436c1f8f48398a
Ezzat Mohamed <73354781+ezat141@users.noreply.github.com>
2024-07-19 16:39:58 +0300

    refresh settings model

--- models/Settings.js
+++ models/Settings.js
@@ -14,6 +14,10 @@ const settingsSchema = new mongoose.Schema({
     settings_bodyhome: {
         type: String,
         required: true
+    },
+    settings_deliverytime: {
+        type: Number,
+        default: 30
     }
 });
 
